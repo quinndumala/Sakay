140ffc3f4beabb670463a812f5d6e60b66806be7
Quinn Dumala <Quinn Dumala>
2016-11-28 22:00:00 +0800

    Add date and time picker

--- app/src/main/java/com/example/quinn/sakay/AddRideOffer.java
+++ app/src/main/java/com/example/quinn/sakay/AddRideOffer.java
@@ -5,9 +5,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.CompoundButton;
 import android.widget.Toast;
 
-public class AddRideOffer extends AppCompatActivity {
+import com.simplicityapks.reminderdatepicker.lib.ReminderDatePicker;
+import com.simplicityapks.reminderdatepicker.lib.OnDateSelectedListener;
+
+import java.text.DateFormat;
+import java.util.Calendar;
+
+public class AddRideOffer extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener{
+
+    private ReminderDatePicker datePicker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,24 +25,28 @@ public class AddRideOffer extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-//        FloatingActionButton fabAddRide = (FloatingActionButton) findViewById(R.id.fabAddRideOffers);
-//        fabAddRide.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-////                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-////                        .setAction("Action", null).show();
-//                Toast.makeText(getApplicationContext(), "Ride Offer Added", Toast.LENGTH_SHORT).show();
-//                finish();
-//
-//            }
-//        });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        datePicker = (ReminderDatePicker) findViewById(R.id.date_picker);
+        datePicker.setOnDateSelectedListener(new OnDateSelectedListener() {
+            @Override
+            public void onDateSelected(Calendar date) {
+                Toast.makeText(AddRideOffer.this, "Selected date: "+ getDateFormat().format(date.getTime()), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
-    @Override
-    public void onBackPressed() {
-
+    private java.text.DateFormat savedFormat;
+    public java.text.DateFormat getDateFormat() {
+        if(savedFormat == null)
+            savedFormat = DateFormat.getDateTimeInstance();
+        return savedFormat;
     }
+
+//    @Override
+//    public void onBackPressed() {
+//
+//    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -54,4 +67,8 @@ public class AddRideOffer extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+    }
 }
