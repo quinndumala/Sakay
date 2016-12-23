b3550814878ffbee287f77fbab09a1701884be0e
Quinn Dumala <Quinn Dumala>
2016-12-23 14:21:13 +0800

    Add Ride Request Activity Functions implemented

---
+++ app/src/main/java/com/example/quinn/sakay/Models/RideRequest.java
@@ -0,0 +1,8 @@
+package com.example.quinn.sakay.Models;
+
+/**
+ * Created by Quinn on 23/12/2016.
+ */
+
+public class RideRequest {
+}
---
+++ app/src/main/java/com/example/quinn/sakay/RideRequestViewHolder.java
@@ -0,0 +1,8 @@
+package com.example.quinn.sakay;
+
+/**
+ * Created by Quinn on 23/12/2016.
+ */
+
+public class RideRequestViewHolder {
+}
